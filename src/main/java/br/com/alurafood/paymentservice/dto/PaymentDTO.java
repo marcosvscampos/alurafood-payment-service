6c85ae32969b3fd85dc64c0103eff86fdbdb3a77
Marcos Campos <contato.marcoscampos@gmail.com>
2023-08-08 15:50:14 -0300

    Remocao de imports não usados

--- src/main/java/br/com/alurafood/paymentservice/dto/PaymentDTO.java
+++ src/main/java/br/com/alurafood/paymentservice/dto/PaymentDTO.java
@@ -1,19 +1,10 @@
 package br.com.alurafood.paymentservice.dto;
 
 import br.com.alurafood.paymentservice.model.Status;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
